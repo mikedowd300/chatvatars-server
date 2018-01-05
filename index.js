41f0c209c020ef28501901c26c222a2ce08f8f86
Mike Dowd <mikedowd1972@gmail.com>
2018-01-04 20:54:46 -0700

    it works, so far

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let client = require('socket.io').listen(4200).sockets
 console.log('I am alive!!');
 
+let usedAvatars = [];
+
 let avatars = [
   'https://c1.staticflickr.com/5/4181/34151964510_60bf24ff96_b.jpg',
   'https://c2.staticflickr.com/4/3726/10412961916_46f51bbf33_b.jpg', 'https://c1.staticflickr.com/3/2848/10413369836_dd1e7018c8_b.jpg', 'https://c2.staticflickr.com/8/7205/6957192282_bcb2f335cd_b.jpg', 'https://c2.staticflickr.com/8/7072/7187991899_5195029315_b.jpg', 'https://c2.staticflickr.com/2/1654/25484519450_8b2ae7d125_b.jpg', 'https://c1.staticflickr.com/6/5571/15087276229_68cc0a324f_z.jpg', 'https://c1.staticflickr.com/9/8273/8709927466_b398900197_b.jpg', 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Ant-Man.jpg', 'http://maxpixel.freegreatpicture.com/Superhero-Comic-Spiderman-Toy-Action-Figure-Hero-1560337', 'https://upload.wikimedia.org/wikipedia/commons/1/13/New_York_Comic_Con_2015_-_The_Vision_%2821916157810%29.jpg', 'https://c1.staticflickr.com/1/485/19905631718_e40c243423_b.jpg', 'https://c1.staticflickr.com/1/746/22391248155_5d614c8f17_b.jpg', 'https://upload.wikimedia.org/wikipedia/commons/8/88/WWE_2014-04-07_20-17-40_NEX-6_1210_DxO_%2813953035184%29.jpg',
@@ -10,10 +12,24 @@ let avatars = [
 client.on('connection', socket => {
   let obj = {
     greeting: 'Howdy!!',
-    avatars
+    avatars,
+    usedAvatars
   };
+
   socket.emit('say-hi', obj);
+
+  socket.on('chatter-added', data => {
+    usedAvatars.push(data);
+    avatars.splice(avatars.indexOf(data), 1);
+    let obj = {
+      avatars,
+      usedAvatars
+    }
+    client.emit('redisplay-avatar-list', obj);
+  });
+
   socket.on('disconnect', data => {
     console.log('goodbye: ', socket.id);
   });
+
 });
